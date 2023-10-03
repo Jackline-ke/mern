1eb011f5cc2c18976e853cba9a3629c1442f37ab
Jackline-ke <antoninahodhiambo@gmail.com>
2023-10-03 16:25:58 +0300

    .env

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,22 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+
+app.use((req, res, next)=>{
+    console.log(req.path, req.method);
+    next()
+});
+
 //listening to a route 
 app.get('/', (req, res) =>{
-    res.status(200).send("Welcome Qlynn to Mern Stack Development");
+    res.send("Hello Qlynn, welcome Mern Stack Development")
+});
+//post request
+app.use(express.json())
+app.post('/', (req, res) =>{
+    const {name} = req.body;
+
+    res.send(`Welcome to Mern Stack ${name}`)
 })
 
 //listening to the port
